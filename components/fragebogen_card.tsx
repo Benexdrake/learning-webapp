2f705c445ddad4ac31f1c31029e7b49cd9849365
BeneX Drake <benex@hotmail.de>
2024-06-14 13:36:46 +0200

    update

--- components/fragebogen_card.tsx
+++ components/fragebogen_card.tsx
@@ -21,14 +21,14 @@ export default function FragebogenCard(params:any)
     }
 
     return (
-        <div className={styles.card} style={{borderColor:borderColor, backgroundColor:question.id%2?'var(--color4)':'var(--color3)'}}>
+        <div className={styles.card} style={{borderColor:borderColor}}>
             <div>            
             <h3 className={styles.card_title}>
                 {question.question}
             </h3>
             {question.answer.map((x:any, i:any) => {
                 return (
-                    <div className={styles.card_answer_block} key={question.answer[i] +  question.id}>
+                    <div className={styles.card_answer_block} key={question.answer[i] +  question.id} style={{backgroundColor:i%2?'var(--color4)':'var(--color3)'}}>
                         <label>
                             <input type='radio' name={question.id} onClick={handleClick} value={i} className={styles.checkmark}/>
                             <span className={styles.card_answer}>{x}</span>
